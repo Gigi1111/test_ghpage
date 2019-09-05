606f569617681b597fe6e714538f302fe2c29cdc
Gigi1111 <airolg1111@gmail.com>
2019-09-06 02:09:04 +0800

    Updates

---
+++ precache-manifest.eee9b1b5948dffac53de81ed8b0ead6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510cefe8c090fbd201dc893d9e8ddbe2",
+    "url": "/test_ghpage/index.html"
+  },
+  {
+    "revision": "ed3c86a2382a0562d85e",
+    "url": "/test_ghpage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a4210f686fb3fb8682d9",
+    "url": "/test_ghpage/static/js/2.fcfad392.chunk.js"
+  },
+  {
+    "revision": "ed3c86a2382a0562d85e",
+    "url": "/test_ghpage/static/js/main.a81c8046.chunk.js"
+  },
+  {
+    "revision": "87e6794ac9499a97d45a",
+    "url": "/test_ghpage/static/js/runtime~main.35170d0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_ghpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
